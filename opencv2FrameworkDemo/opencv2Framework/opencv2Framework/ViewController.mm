0d8b19bd1524788d487a47406a4411c7b6a863dd
kingly <libintm@163.com>
2018-02-06 15:57:22 +0800

    修改

--- opencv2FrameworkDemo/opencv2Framework/opencv2Framework/ViewController.mm
+++ opencv2FrameworkDemo/opencv2Framework/opencv2Framework/ViewController.mm
@@ -7,9 +7,18 @@
 //
 
 #import "ViewController.h"
+
 #import <opencv2/opencv.hpp>
+#import <opencv2/imgproc/types_c.h>
+#import <opencv2/imgcodecs/ios.h>
 
 @interface ViewController ()
+{
+  cv::Mat cvImage;
+}
+
+@property (weak, nonatomic) IBOutlet UIImageView *imgView;
+
 
 @end
 
@@ -19,7 +28,28 @@
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
   
-  //IplImage *img = cvLoadImage(const char *filename)
+  self.view.backgroundColor = [UIColor blackColor];
+  
+  UIImage *image = [UIImage imageNamed:@"learn.jpg"];
+  UIImageToMat(image, cvImage);
+  
+  if(!cvImage.empty()){
+    
+    cv::Mat gray;
+    // 将图像转换为灰度显示
+    cv::cvtColor(cvImage,gray,CV_RGB2GRAY);
+    // 应用高斯滤波器去除小的边缘
+    cv::GaussianBlur(gray, gray, cv::Size(5,5), 1.2,1.2);
+    // 计算与画布边缘
+    cv::Mat edges; cv::Canny(gray, edges, 0, 50);
+    // 使用白色填充
+    cvImage.setTo(cv::Scalar::all(225));
+    // 修改边缘颜色
+    cvImage.setTo(cv::Scalar(0,128,255,255),edges);
+    // 将Mat转换为Xcode的UIImageView显示
+    self.imgView.image = MatToUIImage(cvImage);
+    
+  }
   
 }
 
