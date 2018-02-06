39d13483c52c724a5a8495d1b60e6ee0fac5c903
kingly <libintm@163.com>
2018-02-06 16:20:20 +0800

    添加 Haar特征分类器

---
+++ opencv2FrameworkDemo/opencv2Framework/HaartrainingViewController.h
@@ -0,0 +1,16 @@
+//
+//  HaartrainingViewController.h
+//  opencv2Framework
+//
+//  Created by kingly on 2018/2/6.
+//  Copyright © 2018年 kingly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ 在OpenCV中使用Haar特征分类器来对图像中的人脸进行检测和识别
+ */
+@interface HaartrainingViewController : UIViewController
+
+@end
---
+++ opencv2FrameworkDemo/opencv2Framework/HaartrainingViewController.m
@@ -0,0 +1,37 @@
+//
+//  HaartrainingViewController.m
+//  opencv2Framework
+//
+//  Created by kingly on 2018/2/6.
+//  Copyright © 2018年 kingly. All rights reserved.
+//
+
+#import "HaartrainingViewController.h"
+
+@interface HaartrainingViewController ()
+
+@end
+
+@implementation HaartrainingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
