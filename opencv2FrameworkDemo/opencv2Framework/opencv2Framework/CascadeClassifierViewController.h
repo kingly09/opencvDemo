38bc7f2a3cf8ba2564f3a6e1273311f5cc4c13a4
kingly <libintm@163.com>
2018-02-11 11:02:47 +0800

    使用CascadeClassifier检测人脸

--- opencv2FrameworkDemo/opencv2Framework/opencv2Framework/CascadeClassifierViewController.h
+++ opencv2FrameworkDemo/opencv2Framework/opencv2Framework/CascadeClassifierViewController.h
@@ -11,6 +11,15 @@
 /**
  使用CascadeClassifier检测人脸
  */
+
+/**
+ *
+ 1.分别用facedetect功能将两张图片中的人脸检测出来
+ 2.将人脸部分的图片剪切出来，存到两张只有人脸的图片里。
+ 3.这两张人脸图片转换成单通道的图像
+ 4.使用直方图比较这两张单通道的人脸图像，得出相似度。
+ *
+ **/
 @interface CascadeClassifierViewController : UIViewController
 
 @end
